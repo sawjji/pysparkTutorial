c41d0d0e7675ec7b8b0bc07b5078566aa486a64c
sawjji <yuqiji@umass.edu>
2023-05-01 03:39:50 -0400

    new changes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,6 @@
 import { Box, Image, Text, Flex} from "@chakra-ui/react";
 //import latency from 'src/img/latency.jpg';
-
+import HadoopImg from './hadoopmapreduce.jpg'
 export default function Homepage() {
   return (
     <Box align={'center'} minH="100vh" p={5} bg='#F0EFE2'>
@@ -38,12 +38,46 @@ export default function Homepage() {
           gets recomputed when an action is run on it by default, but an RDD can be persisted into storage in memory or disk. 
         </Text>        
         <Flex mt='25px'></Flex>
+        <Text align={'left'} ms='-3px' width='94%'>All transformations in Spark are considered lazy, in that they don’t compute their results right away. Instead, they just remember 
+        the transformations that are applied to some base dataset. The transformations are only computers when an action requires a result to be returned to the driver program — 
+        such action allows Spark to run more efficiently. By default, every transformed RDD may be recomputed each time an action is run on it. You can also persist an RDD in 
+        memory, using the persist (or cache) method, in which case Spark will keep the elements around on the cluster for much faster access the next time it is queried. 
+        </Text> 
+        <Flex mt='25px'></Flex>
+        <Text align={'left'} ms='-3px' width='94%'>
+        In the example below, the first line is a base RDD from an external file, and the dataset is not loaded in memory. The first line is a pointer to a file, the second line 
+        defines the length as the result of a map transformation (not immediately computed due to the laziness stated above), and when reduce is run, Spark breaks the computation 
+        into tasks to run on separate machines, and each machine runs both its part of the map and a local reduction. Finally, it only returns to answer the driver program.
+        </Text>        
+        <Flex mt='25px'></Flex>
+        <Flex align='horizontal' width='94%'>
+          <Text mt='14px' fontWeight={'bold'}>In [1]:</Text>
+          <Flex w='10px'></Flex>
+          <Box w='94%' bg='black' rounded='sm' p={3} mt='4px' ms='13px' align='left'>
+            <Flex color='white'>
+              <pre>
+                {'lines = sc.textFile("data.txt")\nlineLengths = lines.map(lambda s: len(s))\ntotalLength = lineLengths.reduce(lambda a, b: a + b)'}
+              </pre>
+            </Flex>
+          </Box>
+        </Flex>
+        <Flex mt='15px'></Flex>
         <Text align={'left'} ms='-3px' width='94%'>Hadoop MapReduce is a programming model for processing big datasets with a parallel, distributed algorithm. 
           Hadoop is used extensively to analyze datasets since it is based on MapReduce, a simple programming model. Its computation is scalable, flexible, fault-tolerant, 
           and cost effective. A large challenge for MapReduce is the sequential multi-step process it takes to run a job. For each step, MapReduce reads data from the cluster and 
           performs operations, but since each step requires a disk read and write, MapReduce jobs are slower due to disk I/O latency. 
         </Text>
-        <Text align={'left'} ms='-3px' width='94%'>(image)</Text>
+        <Flex mt='25px'></Flex>
+        <Box boxSize='sm'>
+          <Image src='https://www.tutorialspoint.com/hadoop/images/mapreduce_algorithm.jpg' alt='Dan Abramov' />
+        </Box>
+        <Flex mt='-190px'></Flex>
+        <Text align={'left'} ms='-3px' width='94%'>The MapReduce program executed within three stages: map stage, shuffle stage, and reduce stage. The map/mapper’s job is to process 
+        the input data during the map stage. The input file is passed to the mapper function line by line. For the reduce stage, it turns into a combination of the shuffle stage 
+        and reduce stage, in which the reducer is to process the data that comes from the mapper. After processing the data, a new set of outputs are produced and stored in the HDFS. 
+        During a MapReduce job, Hadoop will send the Map and Reduce tasks to the appropriate servers in the cluster. Most of the computing takes place on nodes with data on local 
+        disks that reduces the network traffic, and after completion of the given tasks, the cluster will collect and reduce the data to form an appropriate result to send back to 
+        the Hadoop server. </Text>
         <Flex mt='25px'></Flex>
         <Text align={'left'} ms='-3px' width='94%'>To address the limitations, Spark was created to process in-memory, reducing the number of steps in a job, and reuse 
           data across multiple parallel operations. Spark reuses data by using an in-memory cache to speed up machine learning algorithms that repeatedly call a function on the same 
